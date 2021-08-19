8dfb1716529dbe98fe7052c7abb871d2d6b4a452
BA_CalderonMorales <tbcm640@gmail.com>
2021-08-19 12:33:38 -0500

    Initial commit

---
+++ MariosConstruction/Models/Owner.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MariosConstruction.Models
+{
+    public class Owner : IdentityUser
+    {
+        [Key]
+        public int OwnerId { get; set; }
+        public string first_name
+    }
+}
---
+++ MariosConstruction/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MariosConstruction.Models
+{
+    public class Review
+    {
+    }
+}
