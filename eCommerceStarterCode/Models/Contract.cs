81a88b2159ac5087d199c30ea318cf6461875184
BA_CalderonMorales <tbcm640@gmail.com>
2021-08-19 14:03:29 -0500

    Downloaded ecommerce starter code from DevCodeCamp GitHub to include a DBContext file for every table to be built using ASP.NET alone.

---
+++ eCommerceStarterCode/Models/Contract.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Contract
+    {
+        [Key]
+        public int ContractId { get; set; }
+        public DateTime Date { get; set; }
+        public string TypeOfWork { get; set; }
+        public string DescriptionOfProject { get; set; }
+        public double CustomerPriceProposal { get; set; }
+        public double Location { get; set; }
+
+        [ForeignKey("Customer")]
+        public string CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
---
+++ eCommerceStarterCode/Models/Owner.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCommerceStarterCode.Models
+{
+    public class Owner : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [ForeignKey("Customer")]
+        public string CustomerId { get; set; }
+        public ICollection<Customer> Customers { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public ICollection<Category> Categories { get; set; }
+
+        [ForeignKey("Contract")]
+        public int ContractId { get; set; }
+        public ICollection<Contract> Contracts { get; set; }
+
+
+    }
+}
